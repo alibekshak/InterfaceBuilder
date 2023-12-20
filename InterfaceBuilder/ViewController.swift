9ae18f8065d8fc6011856772dfe9e8500175a19f
alibekshak <alibekshakiro1@gmail.com>
2023-12-20 10:28:57 +0600

    InterfaceBuilder

---
+++ InterfaceBuilder/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  InterfaceBuilder
+//
+//  Created by Apple on 20.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var intensity: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChange(_ sender: Any) {
+    }
+    
+}
+
